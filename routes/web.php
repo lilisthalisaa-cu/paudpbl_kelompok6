5416277d1800730e4a0e8cf6a5c1c744f9c21faf
Ajeng Maulida <ajengmaulida364@gmail.com>
2026-04-03 14:14:40 +0700

    ajengs

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Auth\UnifiedLoginController;
 use App\Http\Controllers\Admin\TeacherController;
 use App\Http\Controllers\Admin\StudentController;
 use App\Http\Controllers\Admin\RecapController;
+use App\Http\Controllers\Parent\ParentDashboardController;
 
 Route::get('/', function () {
     return redirect('/login');
@@ -23,8 +24,12 @@ Route::middleware('auth')->prefix('admin')->name('admin.')->group(function () {
     Route::resource('/teachers', TeacherController::class);
     Route::resource('/students', StudentController::class);
 
-    // Route::get('/recap/teachers', [RecapController::class, 'teachers'])->name('recap.teachers');
-    // Route::get('/recap/students', [RecapController::class, 'students'])->name('recap.students');
+});
+
+Route::middleware('parent')->prefix('parent')->name('parent.')->group(function () {
+
+    Route::get('/dashboard', [ParentDashboardController::class, 'index'])
+        ->name('dashboard');
 
 });
 

97ac270a25c43b8b2f6a3acfbc927b0b75b78daf
nisaerizkiah <nisaerizkiah@gmail.com>
2026-05-30 13:55:31 +0700

    pembaruan2

--- app/Http/Controllers/Api/ParentGrowthController.php
+++ app/Http/Controllers/Api/ParentGrowthController.php
@@ -3,13 +3,69 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Student;
+use App\Models\DevelopmentNote;
+use Illuminate\Http\Request;
 
 class ParentGrowthController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        $student = Student::where(
+            'nisn',
+            '3221935788'
+        )->first();
+
+        if (!$student) {
+            return response()->json([
+                'data' => []
+            ]);
+        }
+
+        $growths = DevelopmentNote::where(
+            'student_id',
+            $student->id
+        );
+
+        if ($request->month) {
+            $growths->where(
+                'month',
+                $request->month
+            );
+        }
+
+        if ($request->year) {
+            $growths->where(
+                'year',
+                $request->year
+            );
+        }
+
+        $growths = $growths
+            ->orderBy('year', 'desc')
+            ->orderBy('month', 'desc')
+            ->get()
+            ->map(function ($item) {
+
+                return [
+
+                    'date' => $item->month . '/' . $item->year,
+
+                    'month' => (string) $item->month,
+
+                    'year' => (string) $item->year,
+
+                    'description' => (string) $item->description,
+
+                    'tb' => (string) $item->tb,
+
+                    'bb' => (string) $item->bb,
+
+                ];
+            });
+
         return response()->json([
-            'data' => []
+            'data' => $growths
         ]);
     }
 }
